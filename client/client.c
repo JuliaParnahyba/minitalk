96e06f95602e8b5ccc9d89da7cffd8c8cc78e789
jparnahy <prog.jump@gmail.com>
2024-03-07 20:53:35 -0300

    sigaction_fer

--- client/client.c
+++ client/client.c
@@ -3,7 +3,7 @@
 #include <signal.h>
 #include <unistd.h>
 
-void sig_msg(void)
+void sig_msg(int n)
 {
 	printf("mensagem recebida\n\n");
 }
@@ -12,18 +12,19 @@ int main(int c, char **v)
 {
 	if (c == 2)
 	{
-		sig_msg();
 		int n = 10;
 		int server_id = atoi(v[1]);
 		printf("PID server: [%i]\n\n", server_id);
+
+		signal(SIGUSR1, sig_msg);
 		while (n > 0)
 		{
-//			printf("chegou aqui\n");
-			kill(server_id, SIGUSR1);
-//			signal(SIGUSR1, server_id);
-			usleep(600);
-//			printf("passou do kill\n");
+			if (n % 2 == 0)
+				kill(server_id, SIGUSR1);
+			else
+				kill(server_id, SIGUSR2);
 			n--;
+			usleep(600);
 		}
 	}
 	else 
--- server/server.c
+++ server/server.c
@@ -3,23 +3,34 @@
 #include <stdlib.h>
 #include <signal.h>
 
-
-void hello(int n)
+void back_sig(int type_sig, siginfo_t *info, void *context)
 {
-	static int count = 1;
-	printf("value of count: %i\n", count);
+	(void) context;
+
+	static int count = 0;
+	
+	if (type_sig == SIGUSR1)
+		printf("NO SIG1\n\n");
+	if (type_sig == SIGUSR2)
+		printf("NO SIG2\n\n");
 	count++;
-	usleep(600);
+	if (count == 10)
+		kill(info->si_pid, SIGUSR1);
 }
 
 int main(void)
 {
-	printf("%i\n", getpid());
-	hello(1);
-	signal(SIGUSR1, hello);
-	while(1);
+	struct sigaction sa;
+	sa.sa_sigaction = &back_sig;
+	sa.sa_flags = SA_SIGINFO;
 
-	printf("chegou aqui\n");
+	printf("\n ----------- ***** ----------\n\n");
+	printf("QUE COMECEM OS JOGOS!\n");
+	printf("%i\n", getpid());
 	
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+
+	while(1);
 	return (0);
 }
